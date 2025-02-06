4143f63dc6d3a1d4467b1ee3b3c95ba0ce786159
Sakthikumar08 <sakthikumarcricket@gmail.com>
2025-02-06 16:34:25 +0530

    nav updated

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -5,14 +5,16 @@ import "../Styles.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-    
-      <div className="nav-items">
-        <NavLink to="/" className="nav-item">Home</NavLink>
-        <NavLink to="/about" className="nav-item">Project</NavLink>
-        <NavLink to="/mission" className="nav-item">Blog</NavLink>
-       
-      </div>
-    </nav>
+    {/* Logo on the left */}
+    <div className="logo">A</div>
+
+    {/* Navigation items on the right */}
+    <div className="nav-items">
+      <NavLink to="/" className="nav-item">Home</NavLink>
+      <NavLink to="/about" className="nav-item">Project</NavLink>
+      <NavLink to="/mission" className="nav-item">Blog</NavLink>
+    </div>
+  </nav>
   );
 };
 
